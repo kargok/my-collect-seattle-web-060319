cd1d90e48cb3404843799b5f5ee3d835a944a9a2
github username <github email address>
2019-05-25 13:58:39 -0700

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,19 @@
+require 'pry'
 
+def my_collect(arr) # put argument(s) here
+  # code here
+  i = 0
+  out = [];
+  while i < arr.length
+  	yield arr[i]
+  	if arr[i].split(" ").length > 1
+  		out << arr[i].split(" ").first
+  	else
+  		yield arr[i]
+  		out << arr[i].upcase
+  	end
+  	i += 1
+  end
+  out
+end
 
